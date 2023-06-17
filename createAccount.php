9fa8db633f966db62ac8d95e82b2bded490c652f
zzj29 <133968225+zzj29@users.noreply.github.com>
2023-06-17 23:30:48 +1000

    Update createAccount.php

--- createAccount.php
+++ createAccount.php
@@ -1,16 +1,19 @@
+<?php 
+session_start()
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Zhijun Zhang 12.06.2023 ICTPRG302 Project Assessment (Ass#2)">
     <title>Gelos Enterprises</title>
     <link href="style.css" rel="stylesheet">
 </head>
 <body>
     <header>
         <div id="headerContent">
-            
             <nav>
                 <ul>
                     <li class="menu">
@@ -31,46 +34,100 @@
     <main>
         <h1>Create account</h1>
         <?php
-
-        //check the username is provided & valid 
-        //1. if the username is already exist, not able to register
-        //2. valiadation (note case sensitive, 4-40 characters, no space)
-
-        if(isset($_POST["login"]))
+        $NewUser = "";
+        $usernameOK = True;
+        function redirect($URL)
         {
-            if(isset($_POST["username"]))
+            $_SESSION["error"]="<div style='color:red;'>Please supply valid username and password.</div>"; //NOT WORKING ?????????
+            header("Location: " .$URL);
+            exit();
+        }
+
+
+        if(isset($_POST["register"]))
+        {
+            //check the username is provided & valid
+            if(!empty($_POST["username"]))
             {
-                $Name = trim($_POST["username"]); //remove the white space from the username entered
-                if (strlen($Name) < 2)
+                $Name = strtolower(trim($_POST["username"])); //remove the white space & change to lowercase
+                // valid username should only include letters or digit 
+                if (ctype_alnum($Name)) 
                 {
-                    echo "<p>Please supply valid username </p>";
+                    // valid username should be 4-40 characters 
+                    if (strlen($Name) >= 4 && strlen($Name) <= 40)
+                    {
+                        // if the username is already exist, not able to register
+                        $fileArray = file("accounts.txt", FILE_IGNORE_NEW_LINES);
+                        foreach($fileArray as $line)
+                        {
+                            list($storedUsername) = explode(' ', $line);
+                            if ($Name === $storedUsername) 
+                            {
+                                $usernameOK = False;
+                                break;
+                            }
+                        }
+                        if ($usernameOK)
+                        {
+                            $NewUser .= "$Name";
+                        }
+                        else
+                        {
+                            redirect("register.php");
+                        }
+                    }
+                    else
+                    {
+                        redirect("register.php");
+                    }
+                }
+                else
+                {
+                    redirect("register.php");
                 }
             }
-            if(!empty($POST["password"]))
+            else{
+                redirect("register.php");
+            }
+
+            //check the password is provided & valid
+            if(empty($_POST["password"]))
             {
-                $letters=strlen($POST_["password"]);  
+                //if the password in blank, generate a random password with 10 characters
+                $characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*"; 
+                $random = str_shuffle($characters);
+                $password = substr($random, 0, 10);
+                $NewUser .= " $password\n";
+                echo "<p> You are registed. Please record your username is: " . $Name . " and your password is: ". $password . ".</P>"; //show the password to the user to record it
+                // append the new username and password to accounts.txt
+                file_put_contents("accounts.txt", $NewUser, FILE_APPEND);
+            }
+            //if password is provided, validate the password (case sensitive)
+            else
+            {   
+                $password=trim($_POST["password"]); // remove the white space
+                // valid password should only include: numbers (0-9), symbols (!=@#$%&*_()) and letters (a-z,A-Z))
+                if (preg_match("/^[a-zA-Z0-9!=@#$%&*_()]+$/", $password))
+                {
+                    // valid password should be 8-20 characters
+                    if (strlen($password) >= 8 && strlen($password) <= 20)
+                    {
+                        $NewUser .= " $password\n";
+                        echo "<p> You are registed. Please record your username is: " . $Name . " and your password is: ". $password . ".</P>"; //show the password to the user to record it
+                        // append the new username and password to accounts.txt
+                        file_put_contents("accounts.txt", $NewUser, FILE_APPEND);
+                    }  
+                    else
+                    {
+                        redirect("register.php");
+                    }
+                }
+                else
+                {
+                    redirect("register.php");
+                }
             }
         }
-        
-
-        //if the password in blank, generate a random password with 10 characters
-        $characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*"; 
-        $random = str_shuffle($characters);
-        $password = substr($random, 0, 10);
-        echo "<p>$password</p>"; //show the password to the user to record it
-
-
-        //if password is provided, validate the password (case sensitive, 8-20 characters, no space, allow: numbers (0-9), symbols (!=@#$%&*_()) and letters (a-z,A-Z))
-
-
-
-
-        //add the new username and password should be saved to a text file accounts.txt
-
-
-        //inform the user the registration is success
-
-
         ?>
     </main>
     <footer>
