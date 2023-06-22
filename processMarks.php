36630fcbd676bb25a3d8942c7f9d3add2d5c32a3
zzj29 <133968225+zzj29@users.noreply.github.com>
2023-06-22 22:44:59 +1000

    Update processMarks.php

--- processMarks.php
+++ processMarks.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Zhijun Zhang 12.06.2023 ICTPRG302 Project Assessment (Ass#2)">
     <title>Marks</title>
     <link href="style.css" rel="stylesheet">
 </head>
 <body>
     <header>
-        <div id="headerContent">
-            
+        <div id="headerContent">            
             <nav>
                 <ul>
                     <li class="menu">
@@ -31,69 +31,94 @@
     <main>
         <h1>Calculation of your marks</h1>
         <?php
-        //read the 5 marks
-        if(empty($_POST["mark1"]))
-        {
-            echo "<p>Please supply a value for mark 1</p>";
-        }
-        else
-        {
-            $mark1 = (int)$_POST["mark1"];
-        }
 
-        if(empty($_POST["mark2"]))
-        {
-            echo "<p>Please supply a value for mark 2</p>";
-        }
-        else
-        {
-            $mark2 = (int)$_POST["mark2"];
-        }
+            $marks = [];
+            // displyError function to display error message when the input is invalid or empty
+            function displayError($markNumber) {
+            echo "<p>Please supply a valid value for mark $markNumber (between 1 and 100)</p>";
+            }
 
-        if(empty($_POST["mark3"]))
-        {
-            echo "<p>Please supply a value for mark 3</p>";
-        }
-        else
-        {
-            $mark3 = (int)$_POST["mark3"];
-        }
+            // read the 5 marks into an array
+            if(empty($_POST["mark1"])) {
+                displayError(1);
+            } 
+            else {
+                $mark1 = (int)$_POST["mark1"];
+                $marks[] = $mark1;
+                // must ensure that the marks are between 1 and 100 
+                if ($mark1 < 1 || $mark1 > 100) {
+                    displayError(1);
+                }
+            }
 
-        if(empty($_POST["mark4"]))
-        {
-            echo "<p>Please supply a value for mark 4</p>";
-        }
-        else
-        {
-            $mark4 = (int)$_POST["mark4"];
-        }
+            if(empty($_POST["mark2"])) {
+                displayError(2);
+            } 
+            else {
+                $mark2 = (int)$_POST["mark2"];
+                $marks[] = $mark2;
+                // must ensure that the marks are between 1 and 100 
+                if ($mark2 < 1 || $mark2 > 100) {
+                    displayError(2);
+                }
+            }
 
-        if(empty($_POST["mark5"]))
-        {
-            echo "<p>Please supply a value for mark 5</p>";
-        }
-        else
-        {
-            $mark5 = (int)$_POST["mark5"];
-        }
+            if(empty($_POST["mark3"])) {
+                displayError(3);
+            } 
+            else {
+                $mark3 = (int)$_POST["mark3"];
+                $marks[] = $mark3;
+                // must ensure that the marks are between 1 and 100 
+                if ($mark3 < 1 || $mark3 > 100) {
+                    displayError(3);
+                } 
+            }
 
-        //create array with marks
-        $marks = [$mark1, $mark2, $mark3, $mark4, $mark5];
+            if(empty($_POST["mark4"])) {
+                displayError(4);
+            } 
+            else {
+                $mark4 = (int)$_POST["mark4"];
+                $marks[] = $mark4;
+                // must ensure that the marks are between 1 and 100
+                if ($mark4 < 1 || $mark4 > 100) {
+                    displayError(4);
+                }
+            }
 
-        $total = 0;
-        //display the array and calculate sum
-        foreach($marks as $mark)
-        {
-            echo "<p>$mark</p>";
-            $total = $total + $mark;
-        }
-        
-        //calculate the average
-        $average = $total / 5;
+            if(empty($_POST["mark5"])) {
+                displayError(5);
+            } 
+            else {
+                $mark5 = (int)$_POST["mark5"];
+                $marks[] = $mark5;
+                // must ensure that the marks are between 1 and 100
+                if ($mark5 < 1 || $mark5 > 100) {
+                    displayError(5);
+                } 
+            }
 
-        //display sum and average
-        echo "<p>The sum of your marks is: $total</p>";
-        echo "<p>The average of your marks is: $average</p>";
+            // Calculate the sum of marks
+            $total = array_sum($marks);
+            $count = count($marks);
+
+            // Calculate the average
+            if ($count > 0) {
+                $average = $total / $count;
+            } else {
+                $average = 0;
+            }
+
+            // Display the marks
+            foreach($marks as $mark) {
+                echo "<p>$mark</p>";
+            }
+
+            //display sum and average
+            echo "<p>The sum of your marks is: $total</p>";
+            echo "<p>The average of your marks is: $average</p>";
+
         ?>
         
     </main>
